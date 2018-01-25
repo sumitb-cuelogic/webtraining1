2547d317731d33bf74dda94c2259775ccaeebf60
sumit bobade <sumit.bobade@cuelogic.com>
2018-01-25 20:40:13 +0530

    js

---
+++ Documents/javscript_assignment/jsp/profile.js
@@ -0,0 +1,73 @@
+
+ var Allentries;
+var profileobj;
+var userindex;
+function accessprofile()
+{
+
+    console.log("inside ");
+
+    
+     Allentries=JSON.parse(localStorage.getItem("Allentries"));
+    var username=localStorage.getItem("username");
+    var profileobj;
+   
+         userindex=searchUser(username,Allentries);
+
+
+        //alert(userindex.email);
+    
+        document.getElementById("email").value=Allentries[userindex].email;
+        document.getElementById("fname").value=Allentries[userindex].fname;
+        document.getElementById("lname").value=Allentries[userindex].lname;
+        document.getElementById("address").value=Allentries[userindex].address;
+        document.getElementById("image").value=Allentries[userindex].image;
+}
+
+function searchUser(username,Allentries){
+
+    for(var i=0;i<Allentries.length;i++)
+    {
+            if(username==Allentries[i].email)
+            {
+                profileobj=Allentries[i].email;
+                return i;
+            }
+    }
+    return false;
+}
+
+function makeEditable(event){
+
+    event.preventDefault();
+    document.getElementById("email").readOnly=false;
+    document.getElementById("fname").readOnly=false;
+    document.getElementById("lname").readOnly=false;
+    document.getElementById("address").readOnly=false;
+    document.getElementById("image").readOnly=false;
+}
+
+function stopEditable(){
+    document.getElementById("email").readOnly=true;
+    document.getElementById("fname").readOnly=true;
+    document.getElementById("lname").readOnly=true;
+    document.getElementById("address").readOnly=true;
+    document.getElementById("image").readOnly=true;
+
+}
+
+function updateInfo(event){
+
+    event.preventDefault();
+
+        Allentries[userindex].email=document.getElementById("email").value;
+        Allentries[userindex].fname=document.getElementById("fname").value;
+        Allentries[userindex].lname=document.getElementById("lname").value;
+        Allentries[userindex].address=document.getElementById("address").value;
+        Allentries[userindex].image=document.getElementById("image").value;
+        localStorage.setItem("Allentries",JSON.stringify(Allentries[userindex]));
+
+            alert("save changes succefully");
+        stopEditable();
+
+}
---
+++ Documents/javscript_assignment/jsp/signup.js
@@ -0,0 +1,146 @@
+
+
+var flag=false;
+var username="";
+function registerUser(event)
+{
+    event.preventDefault();
+    if(flag!=false)
+    {
+       // location.href="www.google.com";
+        // alert("donechecing");
+        var newdata=adddata();
+        //window.alert("username is "+newdata.email)
+            var Allentries=JSON.parse(localStorage.getItem('Allentries'));
+           
+            if(Allentries==undefined)
+            {
+                Allentries=new Array();
+                
+            }
+
+            Allentries.push(newdata);
+            localStorage.setItem("Allentries",JSON.stringify(Allentries));
+            username=newdata.email;
+            localStorage.setItem("username",username);
+            document.getElementById("formdata").reset();
+            location.href="./profile.html";
+    }    
+    else{
+        alert("Please fill all required fields");
+    }
+}
+    function newuserconst(email,fname,lname,address,password,gender,image)
+    {
+        this.email=email;
+        this.fname=fname;
+        this.lname=lname;
+        this.address=address;
+        this.password=password;
+        this.gender=gender;
+        this.image=image;  
+    }
+    function adddata(){
+        console.log("inside adddata");
+        var email=document.getElementById("email").value;
+        var fname=document.getElementById("fname").value;
+        var lname=document.getElementById("lname").value;
+        var address=document.getElementById("address").value;
+        var password=document.getElementById("passwd1").value;
+        var gender;
+        if(document.getElementById('r1').checked)
+        {
+                gender=document.getElementById("r1").value;
+        }
+        else if(document.getElementById('r2').checked){
+                gender=document.getElementById("r2").value;
+        }
+        else{
+                 gender=document.getElementById("r3").value;
+        }
+        var image=document.getElementById("profimg").value;
+        
+        var newuser= new newuserconst(email,fname,lname,address,password,gender,image);
+
+        return newuser;
+    }
+
+function ValidateEmail(mail) 
+{
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value))
+        {
+            flag=true;
+            mail.style.color="green";
+            return (true)
+        }
+        else{
+
+            flag=false;
+            mail.style.color="red";  
+           return (false);
+        }
+
+
+
+    }
+
+function validatefname(fname){
+
+if(/^[a-zA-Z ]{2,30}$/.test(fname.value))
+    {
+        flag=true;
+        fname.style.color="green";
+            return true;
+    }
+    else{flag=false;
+            fname.style.color="red";
+        return false;
+
+    }
+}
+function validatePassword()
+    {
+        var pass1=document.getElementById("passwd1").value;
+        var pass2=document.getElementById("passwd2").value;
+        if(pass1===pass2)
+        {
+            document.getElementById("passmsg").innerHTML="";
+            flag=true;
+             return true;
+        }
+        else{flag=false;
+          
+           document.getElementById("passmsg").innerHTML="password do not matched !";
+           return false;
+        }
+    }
+
+function validateUserLogin(event){
+
+    event.preventDefault();
+    var username=document.getElementById("usernamel").value;
+    var password=document.getElementById("passwordl").value;
+
+        //alert(username+" "+password);
+
+    var allentries=JSON.parse(localStorage.getItem("Allentries"));
+    var index=searchUser(username,allentries);
+
+    //alert(index);
+
+        if((username==undefined) && (password==undefined))
+        {
+            alert("invaid data");
+        }
+        else if((username===(allentries[index].email)) && (password===(allentries[index].password)))
+        {
+            alert("succesfully login");
+            //location.href="./home.html";
+        }
+        else{
+            alert("invalid data");
+        }
+
+
+
+}
